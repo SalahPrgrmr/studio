de0ab87ae970fc9433dbc2060a26ed5490539866
Bseem Alhidry <balhidry@gmail.com>
2025-11-15 22:29:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9297202409-e39e0",
+  "appId": "1:159841585510:web:1b3b4abbaab408eb8ce0a7",
+  "apiKey": "AIzaSyDMJ_1wGGxa9U9lHk-JRSVJaya8LD4VdKw",
+  "authDomain": "studio-9297202409-e39e0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "159841585510"
+};
