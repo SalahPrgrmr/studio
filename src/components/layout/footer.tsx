be14399e9804f643f49df26e0be698b21334f146
Bseem Alhidry <balhidry@gmail.com>
2025-11-17 20:51:29 +0000

    اصلح الاخطاء واعد التشغيل

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -21,7 +21,7 @@ export default function Footer() {
   useEffect(() => {
     const handleBeforeInstallPrompt = (e: Event) => {
       e.preventDefault();
-      setDeferredPrompt(e as BeforeInstallallPromptEvent);
+      setDeferredPrompt(e as BeforeInstallPromptEvent);
     };
 
     window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
